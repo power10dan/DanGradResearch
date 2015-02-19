3093abad1d45c3286f2eb58d670c3c83660f7b50
power10dan <lintzu@onid.oregonstate.edu>
2015-02-19 01:01:17 -0800

    Remove Redundant Files

--- compile.sh
+++
@@ -1,6 +0,0 @@
-g++ peks.cpp bn_pair.cpp zzn12a.cpp ecn2.cpp zzn4.cpp zzn2.cpp big.cpp zzn.cpp ecn.cpp miracl.a -o original_peks
-
-g++ client.cpp Client_Imp.cpp Company_Imp.cpp bn_pair.cpp zzn12a.cpp ecn2.cpp zzn4.cpp zzn2.cpp big.cpp zzn.cpp ecn.cpp miracl.a -o own_peks
-
-./original_peks
-./own_peks
