d81cfdc64d13e5a2aaca49cb960f85cd7c22b1c8
power10dan <lintzu@onid.oregonstate.edu>
2015-02-19 01:02:55 -0800

    Recommitting Bosch Work

---
+++ Bosch_Work/ToyModel.h
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+// socket includes
+#include<zmq.hpp>
+#include<stdio.h>
+#include<unistd.h>
+// Using Pailler Encryption from MIRACL
+#include "big.h"
+
+class Sockett_Class{
+
+	public:
+		void Send(String);
+		void Receive();
+};
+
+class Client1{
+
+	public:
+		Big Encryption(String);
+};
+
+class Client2{
+
+	public:
+		Big Encrypt_Add(String);
+
+};
+
+class Client3{
+
+	public:
+		Big Encrypt_Add(String);
+
+};
