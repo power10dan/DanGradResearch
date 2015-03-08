57da7bac297b4d1f35fb872d0f792c83bd9102a5
power10dan <lintzu@onid.oregonstate.edu>
2015-03-08 04:06:30 -0700

    Client 1 Can Connect and Talk to Client 2

--- Bosch_Work/Client1_Code/Client1.h
+++ Bosch_Work/Client1_Code/Client1.h
@@ -3,16 +3,20 @@
 
 #include "p1363.h"
 
-typedef struct{
-
+typedef struct{	
+	
 	octet key;
 	octet MAC;
+	octet cipher;
+	// pre-shared keys
+	octet key_2;
+	octet key_3;
+
 } key_mac;
 
 // client function prototypes
-void communicate(octet*);
-void receive(octet*);
-octet encrypt(char*, octet*);
+void communicate(octet);
+key_mac encrypt(char*, key_mac);
 void decrypt(octet*, octet*, octet*);
 
 #endif
--- Bosch_Work/Client1_Code/Client1_Imp.c
+++ Bosch_Work/Client1_Code/Client1_Imp.c
@@ -1,70 +1,81 @@
 // 0MQ
 #include<zmq.h>
+#include<unistd.h>
+// C includes
 #include<stdio.h>
 #include<stdlib.h>
 // With MIRACL includes
 #include "Client1.h"
 
 int main(){
-
-	
-
-
-
-
+	key_mac mac_ciph_pair;
+	mac_ciph_pair = encrypt("TestMessage", mac_ciph_pair);
+	printf("Cipher Text To Be Sent\n");
+	OCTET_OUTPUT(&(mac_ciph_pair.cipher));
+	communicate(mac_ciph_pair.cipher);
 }
 
-octet encrypt(char* message, octet* key){
+key_mac encrypt(char* message, key_mac mac_ciph){
 
-	octet MAC_tag, cipher, plain_txt;
-	key_mac  mac_ciph;
-
+	octet MAC_tag, cipher, plain_txt, key_temp;
+	int i;
 	OCTET_INIT(&MAC_tag, 100);
-	OCTET_INIT(&cipher, 100);
+	OCTET_INIT(&key_temp, 100);
 	OCTET_INIT(&plain_txt, 100);
+	OCTET_INIT(&cipher, 100);	
+	OCTET_INIT(&(mac_ciph.key), 100);
+	OCTET_INIT(&(mac_ciph.MAC), 100);
+	OCTET_INIT(&(mac_ciph.cipher), 100);
 	OCTET_JOIN_STRING(message, &plain_txt); 
-
+	// randomly generate a key
+	key_temp.len = 16;
+	for(i = 0; i < 16; i++){ 
+		key_temp.val[i] = 1;
+	}
+	OCTET_COPY(&key_temp, &(mac_ciph.key));
 	// perform AES encryption in Block CBC-IV0 Mode
-	BOOL res_encrypt = AES_CBC_IV0_ENCRYPT(&key, &plain_txt, 
-								NULL, &cipher,NULL);
+	BOOL res_encrypt = AES_CBC_IV0_ENCRYPT(&key_temp, &plain_txt, NULL, &cipher,NULL);
 	// compute mac address for the cipher text
-	BOOL res= MAC1(&cipher, NULL, &key, 20, SHA256, &MAC_tag);
-	printf("Encrypted word\n");
-	OCTET_JOIN_OCTET(&cipher, &MAC_tag);	
+	BOOL res= MAC1(&cipher, NULL, &key_temp, 20, SHA256, &MAC_tag);	
+	// save MAC address
+	OCTET_COPY(&MAC_tag, &(mac_ciph.MAC));
+	OCTET_JOIN_OCTET(&cipher, &MAC_tag);
+	OCTET_COPY(&MAC_tag, &(mac_ciph.cipher));
+	printf("Encryption Complete\n");	
 
-	return MAC_tag;
+	return mac_ciph;
 }
 
-void decrypt(octet* cipher_txt, octet* key, octet *mac){
+/*void decrypt(octet cipher_txt, octet* key, octet *mac){
 
 	octet mac2, plain_txt;
 	OCTET_INIT(&mac2, 100);
 	OCTET_INIT(&plain_txt, 100); 
-
+	key_mac mac_ciph;
+	
 	// compute  MAC with the received cipher text and shared key
-	BOOL res = MAC1(&cipher_txt, NULL, &key, 20, SHA256, &mac2);
-
+	BOOL res = MAC1(&cipher_txt, NULL, &mac_ciph.key, 20, SHA256, &mac2);
 	// if MAC matches, then decrypt the message and print out plain text
 	if(OCTET_COMPARE(&mac, &mac2)){
-
 		BOOL res_decrypt = AES_CBC_IV0_DECRYPT(&key, &cipher_txt,
 								NULL, &plain_txt,NULL);
 		printf("My plain text: ");
 		OCTET_OUTPUT_STRING(&plain_txt);
-
-	} else{
+	}else{
 		printf("Wrong MAC\n");
 	}
 
-}
+}*/
 
-void communicate(octet* MAC_cipher){
+void communicate(octet MAC_cipher){
+	void *context = zmq_ctx_new();
+	void *requester = zmq_socket (context, ZMQ_REQ);
+	zmq_connect(requester, "tcp://192.168.38.140:5555");
 
-	// set up client to send package
-	
-	
-
-
-
+	zmq_send(requester, MAC_cipher.val, 100,0); 
+	printf("Cipher text sending...\n");
+	zmq_close(requester);
+	zmq_ctx_destroy(context);
+	printf("Cipher text sent\n");
 }
 
