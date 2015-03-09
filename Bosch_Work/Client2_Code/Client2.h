00a687623501c43f63a277c5d2df80962e2d22f6
power10dan <lintzu@onid.oregonstate.edu>
2015-03-09 08:28:35 -0700

    Client 2 Can Receive Message, Encrypt A Plain Text, and Respond

--- Bosch_Work/Client2_Code/Client2.h
+++ Bosch_Work/Client2_Code/Client2.h
@@ -5,19 +5,22 @@
 
 typedef struct{
 
-	octet key; // client 2's unique key
-	octet MAC;
-	octet cipher;
-	
-	// pre-shared keys for various clients
+	octet cipher;	
+	// unique key to current client
+	octet key; 
+	// shared keys
 	octet key_1;
 	octet key_3;
+	// mac address of each client
+	octet MAC;
+	octet MAC_1;
+	octet MAC_3;
 
 } key_mac;
 
-// server ops 
-void communicate();
-octet encrypt();
-void decrypt();
+void communicate(key_mac);
+key_mac key_gen(key_mac, int, int);
+key_mac encrypt(char*, key_mac);
+void decrypt(octet, key_mac);
 
 #endif
